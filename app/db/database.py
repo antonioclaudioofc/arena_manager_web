42539617918999ee55c4a6c8ea8b57accb4ef050
Antonio Claudio <claudioalvesdev@gmail.com>
2025-11-28 10:55:34 -0300

    feat: change database

--- app/db/database.py
+++ app/db/database.py
@@ -4,8 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from core.config import SQLALCHEMY_DATABASE_URL
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
-                       'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
